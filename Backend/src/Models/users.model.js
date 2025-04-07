42afbe74bbf796194a8c0a49da2c351d42f047cb
Mandeep Sharma <mandeepsharma0701@gmail.com>
2025-04-07 18:14:10 +0530

    Add user model and jwt

---
+++ Backend/src/Models/users.model.js
@@ -0,0 +1,112 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+const { Schema } = mongoose;
+
+const userSchema = new Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      match: [/\S+@\S+\.\S+/, "Please enter a valid email"],
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    bio: {
+      type: String,
+      default: "Hey there! I'm Using ChatApp",
+    },
+    phoneNo: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      match: [/^\d{10}$/, "Phone number must be exactly 10 digits"],
+    },
+    password: {
+      type: String,
+      required: true,
+      match: [
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,16}$/,
+        "Password must be 8-16 characters and contain only letters, numbers, @, -, or _",
+      ],
+    },
+    reportNo: {
+      type: Number,
+      default: 0,
+      min: 0,
+    },
+    blockedUser: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    friends: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    avatar: {
+      type: String,
+    },
+
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+//hooks
+userSchema.pre("save", async function (next) {
+  //This code of the block always run when you save any user , like if you edit the user in future then before saving user we run this logic.
+  //if password wan't change then don't run the logic
+  if (!this.isModified("password")) return next();
+
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+//Custom methods
+userSchema.methods.isPasswordCorrect = async function (password) {
+  //take two 1.string Password and database stored one.
+  return await bcrypt.compare(password, this.password);
+  //compare return true or false
+};
+
+userSchema.methods.generateAccessToken = async function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      name: this.name,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  );
+};
+
+userSchema.methods.generateRefreshToken = async function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    }
+  );
+};
+
+export const User = model("User", userSchema);
